e14215d1fc157b254364e3dc8aa75103d4944250
katie_code_git <252997438+katie-coder-git@users.noreply.github.com>
2026-08-16 22:13:54 -0700

    finished until 7. No extra Credit

--- script.js
+++ script.js
@@ -5,45 +5,122 @@ function roundtoDecimals(num) {
 
 
 const operators = {
-    add: function (numberA, numberB) {
-        return numberA + numberB;
+    "+": function (nA, nB) {
+        return nA + nB;
     },
-    subtract: function (numberA, numberB) {
-        return numberA - numberB;
+    "-": function (nA, nB) {
+        return nA - nB;
     },
-    multiply: function (numberA, numberB) {
-        return roundtoDecimals(numberA*numberB);
+    "x": function (nA, nB) {
+        return roundtoDecimals(nA*nB);
     },
-    divide: function (numberA, numberB) {
-        if (numberB === 0) {
+    "/": function (nA, nB) {
+        if (nB === 0) {
             return NaN;
         }
         else {
-            return roundtoDecimals(numberA/numberB);
+            return roundtoDecimals(nA/nB);
         }
     }
 }
 
 
 
-function operate (numA, numB, operator) {
-    return operators[operator](numA, numB);
+function operate (numberA, numberB, opr) {
+    return operators[opr](numberA, numberB);
 }
 
 
-let numA = 0;
-let numB = 0;
-let operator ;
+
+
+function calculate (numberA, numberB, opr) {
+    outcomeNum = operate(numberA, numberB, opr);
+    displayValue(outcomeNum);
+    previousOutcomeNum = outcomeNum;
+    numStrA = '';
+    numStrB = '';    
+    operator = '';
+}
+
+function displayReset () {
+    const outputReset = 0;
+    displayValue(outputReset);
+}
+
+function displayValue(numOrStr) {
+    display.textContent = String(numOrStr);
+}
+
+
+let numStrA = '' ;
+let numStrB = '';
+let operator = '';
+let previousOutcomeNum = 0;
+
 
 const display = document.querySelector(".display");
-const numberButtons = document.querySelectorAll(".number");
+displayReset ();
 
-numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener( "click", (e) => {
-        numA = e.target.textContent;
-        display.textContent = e.target.textContent;
+const numStrButtons = document.querySelectorAll(".numStr");
+const operatorButtons = document.querySelectorAll(".operator")
+const equalButton = document.querySelector(".equal");
+const clearButton = document.querySelector(".clear");
+
+function concatNumStr (numStr1, numStr2) {
+    if (numStr1 === '0') {
+        return '' + numStr2;
+    } else {
+        return numStr1 + numStr2;
+    }
+}
+
+
+numStrButtons.forEach((numStrButton) => {
+    numStrButton.addEventListener( "click", (e) => {
+        if (operator === '' ) {
+            numStrA = concatNumStr(numStrA, e.target.textContent);
+            displayValue(numStrA);
+        } else {
+            numStrB = concatNumStr(numStrB, e.target.textContent);
+            displayValue(numStrB);
+        }
+       
     })
 });
+
+operatorButtons.forEach((operatorButton) => {
+    operatorButton.addEventListener("click", (e) => {
+        if (numStrA !== '' && operator !=='' && numStrB !== '' ) {
+            calculate(Number(numStrA), Number(numStrB), operator);        
+        }
+
+        if (numStrA === '' && operator ==='' && numStrB ==='') {
+            numStrA = String(previousOutcomeNum);
+        }
+
+        operator = e.target.textContent;
+    })
+});
+
+equalButton.addEventListener("click", (e) => {
+
+    if (numStrA && operator && numStrB ) {
+        calculate(Number(numStrA), Number(numStrB), operator);
+    }
+});
+
+clearButton.addEventListener("click", (e) => {
+    numStrA = '';
+    numStrB = '';
+    operator = '';
+    previousOutcomeNum = 0;
+    displayReset();
+})
+
+
+
+
+
 
 
 
